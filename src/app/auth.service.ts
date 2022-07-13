c529654ad59332ab573cb3fa24bc1449b7ca8cbd
abirg <abirg@DESKTOP-EROSOO1>
2022-07-13 15:19:24 +0100

    signInConfiguration

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { of } from 'rxjs';
+import { of, Subject } from 'rxjs';
 
+export interface User {
+  email:string;
+  fullname:string;
+  password:string
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
+  private user$ = Subject<User>();
   constructor() { }
 
   login(password: string, email: FormControl<string | null>) {
     console.log("connected");
     return of({email,password});
+  }
+  get user(){return this.user$.asObservable();}
+
+  register(user:any){ 
+    this.user$.next(user);
+    return of(user);
   }
 
   
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators,FormGroupDirective, NgForm,} from '@angular/forms';
+import {FormControl, Validators,FormGroupDirective, NgForm, FormGroup,} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,19 +11,33 @@ import {ErrorStateMatcher} from '@angular/material/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private authService:AuthService) { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    const user=this.userForm.getRawValue();
+    this.authService.register(user).subscribe(s=>this.router.navigate(['/login']));
+  }
+
+
+
+  userForm=new FormGroup({
+    firstname : new FormControl('',[Validators.required]),
+    lastsname : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required])
+  });
+
   email = new FormControl('', [Validators.required, Validators.email]);
+  /*l'inscription ne se fait pas jusqu'au l'entré d'un mail valide*/ 
   /*getErrorMessage() {
       if (this.email.hasError('required')) {
         return 'You must enter a value';
       }
-  
       return this.email.hasError('email') ? 'Not a valid email' : '';
     }*/
+    
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   matcher = new MyErrorStateMatcher();
   hide = true;
